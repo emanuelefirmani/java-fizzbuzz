e3be05c19b21d15fee102f87e5f0ea3c8d8a841c
Emanuele Firmani <emanuelefirmani@kpmg.com>
2020-03-30 11:07:47 +0200

    prints expected output

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -1,4 +1,20 @@
 package org.example;
 
 public class FizzBuzz {
+    public static void main(String[] args) {
+        FizzBuzz fb = new FizzBuzz();
+        fb.calculate(15);
+    }
+
+    public void calculate(int elements) {
+        for (int i = 1; i <= elements; i++) {
+            if(i % 3 == 0)
+                System.out.print("Fizz");
+            if(i % 5 == 0)
+                System.out.print("Buzz");
+            if(i % 3 != 0 && i % 5 != 0)
+                System.out.print(i);
+            System.out.println();
+        }
+    }
 }
