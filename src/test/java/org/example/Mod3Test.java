6c3633521317bf7606faff52f4dbc4c41fbc6805
Emanuele Firmani <emanuelefirmani@kpmg.com>
2020-03-31 14:14:22 +0200

    Mod3 can calculate multiples of 3

---
+++ src/test/java/org/example/Mod3Test.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
+public class Mod3Test {
+    @Test
+    public void should_calculate_multiples_3() {
+        Mod3 sut = new Mod3();
+        int[] values = new int[] {3,6,9,12,18,21,24,27};
+        for (int value : values) {
+            String actual = sut.calculate(value);
+            assertThat(actual).isEqualTo("Fizz");
+        }
+    }
+}
