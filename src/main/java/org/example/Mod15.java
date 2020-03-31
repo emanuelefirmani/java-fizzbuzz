2204cc367385a7e8e1d625aa97ed4564fa2b92fa
Emanuele Firmani <emanuelefirmani@kpmg.com>
2020-03-31 12:05:25 +0200

    Mod15 implements chain of responsibility

--- src/main/java/org/example/Mod15.java
+++ src/main/java/org/example/Mod15.java
@@ -1,7 +1,16 @@
 package org.example;
 
 public class Mod15 implements ICalculator {
+    private ICalculator mockedChain;
+
+    public Mod15(ICalculator mockedChain) {
+
+        this.mockedChain = mockedChain;
+    }
+
     public String calculate(int value) {
-        return "FizzBuzz";
+        if(value % 15 == 0)
+            return "FizzBuzz";
+        return mockedChain.calculate(value);
     }
 }
--- src/test/java/org/example/Mod15Test.java
+++ src/test/java/org/example/Mod15Test.java
@@ -1,17 +1,33 @@
 package org.example;
 
 import org.junit.Test;
-
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class Mod15Test {
     @Test
     public void should_calculate_multiples_15() {
-        Mod15 sut = new Mod15();
+        Mod15 sut = new Mod15(null);
         int[] values = new int[] {15,30,45,60,75};
         for (int value : values) {
             String actual = sut.calculate(value);
             assertThat(actual).isEqualTo("FizzBuzz");
         }
+    }
+
+    @Test
+    public void should_call_next_chain() {
+        ICalculator mockedChain = mock(ICalculator.class);
+        Mod15 sut = new Mod15(mockedChain);
+
+        int[] values = new int[] {1,2,1000,-50};
+        for (int value : values) {
+            String actual = sut.calculate(value);
+            verify(mockedChain).calculate(value);
+        }
+
+        when(mockedChain.calculate(100)).thenReturn("output");
+        String actual = sut.calculate(100);
+        assertThat(actual).isEqualTo("output");
     }
 }
