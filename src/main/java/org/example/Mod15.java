7afbb4c60c94c214b0e02290979f3d75fcd00cd5
Emanuele Firmani <emanuelefirmani@kpmg.com>
2020-03-31 16:19:50 +0200

    Properties are renamed

--- src/main/java/org/example/Mod15.java
+++ src/main/java/org/example/Mod15.java
@@ -1,16 +1,16 @@
 package org.example;
 
 public class Mod15 implements ICalculator {
-    private ICalculator mockedChain;
+    private ICalculator next;
 
-    public Mod15(ICalculator mockedChain) {
+    public Mod15(ICalculator next) {
 
-        this.mockedChain = mockedChain;
+        this.next = next;
     }
 
     public String calculate(int value) {
         if(value % 15 == 0)
             return "FizzBuzz";
-        return mockedChain.calculate(value);
+        return next.calculate(value);
     }
 }
--- src/main/java/org/example/Mod3.java
+++ src/main/java/org/example/Mod3.java
@@ -1,17 +1,17 @@
 package org.example;
 
 public class Mod3 implements ICalculator {
-    private ICalculator nextChain;
+    private ICalculator next;
 
-    public Mod3(ICalculator nextChain) {
+    public Mod3(ICalculator next) {
 
-        this.nextChain = nextChain;
+        this.next = next;
     }
 
     @Override
     public String calculate(int value) {
         if(value % 3 == 0)
             return "Fizz";
-        return nextChain.calculate(value);
+        return next.calculate(value);
     }
 }
