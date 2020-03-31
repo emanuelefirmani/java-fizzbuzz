4e3eafe516581f451ca68376f307ab0c8c7681da
Emanuele Firmani <emanuelefirmani@kpmg.com>
2020-03-31 16:28:21 +0200

    Mod5 implements chain of responsibility

--- src/main/java/org/example/Mod5.java
+++ src/main/java/org/example/Mod5.java
@@ -1,8 +1,16 @@
 package org.example;
 
 public class Mod5 implements ICalculator {
+    private ICalculator next;
+
+    public Mod5(ICalculator next) {
+        this.next = next;
+    }
+
     @Override
     public String calculate(int value) {
-        return "Buzz";
+        if(value % 5 == 0)
+            return "Buzz";
+        return next.calculate(value);
     }
 }
--- src/test/java/org/example/Mod5Test.java
+++ src/test/java/org/example/Mod5Test.java
@@ -7,11 +7,27 @@ import static org.mockito.Mockito.*;
 public class Mod5Test {
     @Test
     public void should_calculate_multiples_5() {
-        Mod5 sut = new Mod5();
+        Mod5 sut = new Mod5(null);
         int[] values = new int[] {5,10,20,25,35,40,50,55};
         for (int value : values) {
             String actual = sut.calculate(value);
             assertThat(actual).isEqualTo("Buzz");
         }
+    }
+
+    @Test
+    public void should_call_next_chain() {
+        ICalculator mockedChain = mock(ICalculator.class);
+        Mod5 sut = new Mod5(mockedChain);
+
+        int[] values = new int[] {1,2,999,-51};
+        for (int value : values) {
+            String actual = sut.calculate(value);
+            verify(mockedChain).calculate(value);
+        }
+
+        when(mockedChain.calculate(99)).thenReturn("output");
+        String actual = sut.calculate(99);
+        assertThat(actual).isEqualTo("output");
     }
 }
