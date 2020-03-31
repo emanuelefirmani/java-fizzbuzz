acafa18565f355c4c47ba99fb100866b39b01835
Emanuele Firmani <emanuelefirmani@kpmg.com>
2020-03-31 16:18:43 +0200

    Mod3 implements chain of responsibility

--- src/main/java/org/example/Mod3.java
+++ src/main/java/org/example/Mod3.java
@@ -1,8 +1,17 @@
 package org.example;
 
 public class Mod3 implements ICalculator {
+    private ICalculator nextChain;
+
+    public Mod3(ICalculator nextChain) {
+
+        this.nextChain = nextChain;
+    }
+
     @Override
     public String calculate(int value) {
-        return "Fizz";
+        if(value % 3 == 0)
+            return "Fizz";
+        return nextChain.calculate(value);
     }
 }
--- src/test/java/org/example/Mod3Test.java
+++ src/test/java/org/example/Mod3Test.java
@@ -8,11 +8,27 @@ import static org.mockito.Mockito.*;
 public class Mod3Test {
     @Test
     public void should_calculate_multiples_3() {
-        Mod3 sut = new Mod3();
+        Mod3 sut = new Mod3(null);
         int[] values = new int[] {3,6,9,12,18,21,24,27};
         for (int value : values) {
             String actual = sut.calculate(value);
             assertThat(actual).isEqualTo("Fizz");
         }
+    }
+
+    @Test
+    public void should_call_next_chain() {
+        ICalculator mockedChain = mock(ICalculator.class);
+        Mod3 sut = new Mod3(mockedChain);
+
+        int[] values = new int[] {1,2,1000,-50};
+        for (int value : values) {
+            String actual = sut.calculate(value);
+            verify(mockedChain).calculate(value);
+        }
+
+        when(mockedChain.calculate(100)).thenReturn("output");
+        String actual = sut.calculate(100);
+        assertThat(actual).isEqualTo("output");
     }
 }
