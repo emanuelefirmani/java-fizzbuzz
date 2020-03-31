c85ed9a196e903a5b8a75a8eeabf943f54917a67
Emanuele Firmani <emanuelefirmani@kpmg.com>
2020-03-31 16:40:55 +0200

    prints expected output

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -1,5 +1,11 @@
 package org.example;
 
 public class FizzBuzz {
+    public static void main(String[] args) {
+        Chain chain = new Chain();
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(chain.calculate(i));
+        }
+    }
 }
 
